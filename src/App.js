ba0f3afa7f7ad82e9d5a50dfe261413b11b64d9a
Elisha Lucile O <lucile.ong@my.utsa.edu>
2023-12-28 20:01:31 -0600

    Migration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
 
+//TODO: Change to router
 function App() {
+  let component
+  switch(window.location.pathname) {
+    case "/":
+      component = <Home />
+      break
+    case "/projects":
+      component = <Projects />
+      break
+    case "/about":
+      component = <About />
+      break
+    default:
+      component = <Home />
+      break
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <Navbar />
+    {component}
+   </>
   );
 }
 
