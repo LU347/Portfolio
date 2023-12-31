5cd7f07346dcebb49306015e40860919f99ef399
Elisha Lucile O <lucile.ong@my.utsa.edu>
2023-12-30 23:53:50 -0600

    Card setup

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,3 +1,5 @@
+import ttrImage from '../images/ttr.png';
+
 const Projects = () => {
     let component
     function handleClick(event) {
@@ -10,16 +12,44 @@ const Projects = () => {
             projectContainer.innerHTML = "Personal projects:"
         }
     }
-
     return ( 
-        <div className="projectsNav">
+        <div className="projects">
             <div class="container">
-                <div class="project-nav">
-                        <button class="project-button" id="classProjects" onClick={handleClick}>College</button>
-                        <button class="project-button" id="personalProjects" onClick={handleClick}>Personal</button>
+                <div class="project-grid-wrapper">
+                    <div class="card">
+                        <a href="https://study-at.vercel.app/"><img src={ttrImage}></img></a>
+                        <div class="gradient-overlay"></div>
+                        <div class="project-info">
+                            <h4>Fall 2023</h4>
+                            <h3>Study with Me</h3>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="https://github.com/NicoletteAL/All-Fangs-On-Deck"><img src={ttrImage}></img></a>
+                        <div class="gradient-overlay"></div>
+                        <div class="project-info">
+                            <h4>Fall 2023</h4>
+                            <h3>All Fangs on Deck</h3>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="https://www.roblox.com/games/13462285447/Teapot-Tycoon-Remastered-Alpha-v1-77" target="_blank" rel="noopener noreferrer"><img src={ttrImage}></img></a>
+                        <div class="gradient-overlay"></div>
+                        <div class="project-info">
+                            <h4>Fall 2023</h4>
+                            <h3>Botanical Buddies</h3>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="https://www.roblox.com/games/13462285447/Teapot-Tycoon-Remastered-Alpha-v1-77" target="_blank" rel="noopener noreferrer"><img src={ttrImage}></img></a>
+                        <div class="gradient-overlay"></div>
+                        <div class="project-info">
+                            <h4>SUMMER 2023</h4>
+                            <h3>Teapot Tycoon Remastered</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="container" id="project-container"></div>
         </div>
      );
 }
