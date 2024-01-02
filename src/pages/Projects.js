076eec27a3466ddc2737bbbdbb9593f6730e2fb6
LU347 <103535661+LU347@users.noreply.github.com>
2024-01-01 19:45:58 -0600

    Project page (#3)
    * Card setup
    
    * Project Cards
    
    * Background update
    
    missing the noise
    
    * Noise gradient

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,28 +9,28 @@ const Projects = () => {
             <div class="container">
                 <div class="project-grid-wrapper" id="project-list">
                     <div class="card">
-                        <a href="https://study-at.vercel.app/"><img src={studyImage} alt="study with me"></img></a>
+                        <a href="https://study-at.vercel.app/" target="_blank" rel="noopener noreferrer"><img src={studyImage} alt="study with me"></img></a>
                         <div class="project-info">
                             <h4>Fall 2023</h4>
                             <h3>Study with Me</h3>
                         </div>
                     </div>
                     <div class="card">
-                        <a href="https://github.com/NicoletteAL/All-Fangs-On-Deck"><img src={allfangsImage} alt="all fangs on deck"></img></a>
+                        <a href="https://github.com/NicoletteAL/All-Fangs-On-Deck" target="_blank" rel="noopener noreferrer"><img src={allfangsImage} alt="all fangs on deck"></img></a>
                         <div class="project-info">
                             <h4>Fall 2023</h4>
                             <h3>All Fangs on Deck</h3>
                         </div>
                     </div>
                     <div class="card">
-                        <a href="https://github.com/LU347/Botanical-Buddies"><img src={botanicalImage} alt="botanical-buddies"></img></a>
+                        <a href="https://github.com/LU347/Botanical-Buddies" target="_blank" rel="noopener noreferrer"><img src={botanicalImage} alt="botanical-buddies"></img></a>
                         <div class="project-info">
                             <h4>Fall 2023</h4>
                             <h3>Botanical Buddies</h3>
                         </div>
                     </div>
                     <div class="card">
-                        <a href="https://www.roblox.com/games/13462285447/Teapot-Tycoon-Remastered-Alpha-v1-77"><img src={ttrImage} alt="teapot tycoon"></img></a>
+                        <a href="https://www.roblox.com/games/13462285447/Teapot-Tycoon-Remastered-Alpha-v1-77" target="_blank" rel="noopener noreferrer"><img src={ttrImage} alt="teapot tycoon"></img></a>
                         <div class="project-info">
                             <h4>Summer 2023</h4>
                             <h3>Teapot Tycoon Remastered</h3>
